f7ca911d71d459178f1bbf562248af9a5ab7f0e7
Lordjoel11 <130599087+Lordjoel11@users.noreply.github.com>
2026-04-23 10:26:32 -0300

    Add files via upload

---
+++ distribuidora/src/test/java/com/Districto_Tech/distribuidora/DistribuidoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Districto_Tech.distribuidora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistribuidoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
