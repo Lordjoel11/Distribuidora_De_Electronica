9d0863e8ca4c45535840a7f1189377a3f5dec079
Lordjoel11 <130599087+Lordjoel11@users.noreply.github.com>
2026-04-30 16:14:54 -0300

    Una breve modificacion en shippingEntity.

--- src/main/java/com/Districto_Tech/distribuidora/features/shipping/shippingEntity.java
+++ src/main/java/com/Districto_Tech/distribuidora/features/shipping/shippingEntity.java
@@ -1,25 +1,28 @@
 package com.Districto_Tech.distribuidora.features.shipping;
 import java.time.LocalDate;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "shipping")
-@Getter
-@Setter
-@ToString
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
 
+@Table(name = "shipping")
+
 public class shippingEntity {
 
-    shippingStatus status;
-    int idShipping;
-    LocalDate date;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    private shippingStatus status;
+    private int idShipping;
+    private LocalDate date;
 
 
 
