4aab3b287eda80806e4f6f6222bff7481049e575
Ivan <ivandiclementejesus@gmail.com>
2026-04-30 10:08:30 -0300

    ClientType ENUM, ClientEntity Class, Adress Class.

--- src/main/java/com/Districto_Tech/distribuidora/features/clients/ClientEntity.java
+++ src/main/java/com/Districto_Tech/distribuidora/features/clients/ClientEntity.java
@@ -1,24 +1,41 @@
 package com.Districto_Tech.distribuidora.features.clients;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Objects;
 
 @AllArgsConstructor
 @Getter
 @ToString
+@Builder
+@Setter
 public class ClientEntity {
 
+    @Setter(AccessLevel.NONE)
     private int clientID;
     private String name;
     private String surname;
+    @Setter(AccessLevel.NONE)
     private String DNI;
     private ClientType clientType;
     private String phoneNumber;
     private Adress homeAdress;
+    @Setter(AccessLevel.NONE)
     private int userID;
 
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ClientEntity that = (ClientEntity) o;
+        return clientID == that.clientID && userID == that.userID;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(clientID, userID);
+    }
 }
