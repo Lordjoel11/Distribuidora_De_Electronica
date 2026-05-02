a178e02b6e58e93fb26055ca96edcf96faed887c
Lordjoel11 <130599087+Lordjoel11@users.noreply.github.com>
2026-05-02 17:22:02 -0300

    Creacion de Exceptions User y handler.

---
+++ src/main/java/com/Districto_Tech/distribuidora/common/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.Districto_Tech.distribuidora.common.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
