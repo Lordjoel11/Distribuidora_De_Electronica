2c60f8ce67284ad639e29dc883ecc36edba26028
Lordjoel11 <130599087+Lordjoel11@users.noreply.github.com>
2026-06-12 20:07:03 -0300

    Delete src/main/java/com/Districto_Tech/distribuidora/common directory

--- src/main/java/com/Districto_Tech/distribuidora/common/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.Districto_Tech.distribuidora.common.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/Districto_Tech/distribuidora/common/exceptions/UserNotFountException.java
+++
@@ -1,7 +0,0 @@
-package com.Districto_Tech.distribuidora.common.exceptions;
-
-public class UserNotFountException extends RuntimeException {
-    public UserNotFountException(String message) {
-        super(message);
-    }
-}
