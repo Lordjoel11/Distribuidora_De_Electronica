fb43f8a04e09b8164a1a28630a1502b1561423e3
IvanDiClemente <ivandiclementejesus@gmail.com>
2026-06-13 19:38:43 -0300

    Updated ClientResponseDTO

--- src/main/java/com/Districto_Tech/distribuidora/features/clients/dto/ClientResponseDTO.java
+++ src/main/java/com/Districto_Tech/distribuidora/features/clients/dto/ClientResponseDTO.java
@@ -3,6 +3,5 @@ package com.Districto_Tech.distribuidora.features.clients.dto;
 import com.Districto_Tech.distribuidora.features.clients.Misc.TypeClient;
 
 public record ClientResponseDTO(TypeClient typeClient,
-                                Long phoneNumber,
-                                String direction) {
+                                UserEntity userEntity) {
 }
