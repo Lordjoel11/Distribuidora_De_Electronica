f4bfe9011152555505da7afde1fac7f6624766d7
macb9613-tech <macb9613@gmail.com>
2026-06-14 00:12:06 -0300

    Corrijo controller y aplico IService en el servicio.

--- src/main/java/com/Districto_Tech/distribuidora/features/products/dto/ProductRequestDto.java
+++ src/main/java/com/Districto_Tech/distribuidora/features/products/dto/ProductRequestDto.java
@@ -9,7 +9,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class ProductRequest {
+public class ProductRequestDto {
 
     @NotBlank(message = "El nombre es obligatorio")
     @Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres")
