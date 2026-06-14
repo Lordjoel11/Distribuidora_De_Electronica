da08432c7fc6c5157b0b72009df53d3a15a12371
macb9613-tech <macb9613@gmail.com>
2026-06-14 01:08:45 -0300

    creacion de PaymentMethod y el enum PaymentMethodType

---
+++ src/main/java/com/Districto_Tech/distribuidora/features/payments/PaymentMethod.java
@@ -0,0 +1,24 @@
+package com.Districto_Tech.distribuidora.features.payments;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "payment_methods")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaymentMethod {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(unique = true, nullable = false)
+    private PaymentMethodType name;
+
+    private String description;
+}
---
+++ src/main/java/com/Districto_Tech/distribuidora/features/payments/PaymentMethodType.java
@@ -0,0 +1,8 @@
+package com.Districto_Tech.distribuidora.features.payments;
+
+public enum PaymentMethodType {
+    CASH,
+    CREDIT_CARD,
+    DEBIT_CARD,
+    TRANSFER
+}
