0ab7c92f65539ae80151b3c6b239f512c7383bb9
IvanDiClemente <ivandiclementejesus@gmail.com>
2026-06-14 00:02:08 -0300

    Finished POSTmapping for ClientController

--- src/main/java/com/Districto_Tech/distribuidora/features/clients/ClientController.java
+++ src/main/java/com/Districto_Tech/distribuidora/features/clients/ClientController.java
@@ -1,4 +1,31 @@
 package com.Districto_Tech.distribuidora.features.clients;
 
+
+import com.Districto_Tech.distribuidora.features.clients.dto.ClientRequestDTO;
+import com.Districto_Tech.distribuidora.features.clients.dto.ClientResponseDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/clients")
+@RequiredArgsConstructor
 public class ClientController {
+
+    private final ClientService clientService;
+
+
+    @PostMapping
+    public ResponseEntity<ClientResponseDTO> createClient(@Valid @RequestBody ClientRequestDTO clientRequestDTO){
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(clientService.createClient(clientRequestDTO));
+
+    }
+
+
 }
