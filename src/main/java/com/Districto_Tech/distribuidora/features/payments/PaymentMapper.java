ec6e597298bbecc2ca81db99781608a7a322c867
macb9613-tech <macb9613@gmail.com>
2026-06-15 23:33:43 -0300

    Implemetacion del IModelMapper a Payment.

---
+++ src/main/java/com/Districto_Tech/distribuidora/features/payments/PaymentMapper.java
@@ -0,0 +1,32 @@
+package com.Districto_Tech.distribuidora.features.payments;
+
+import com.Districto_Tech.distribuidora.common.IModelMapper;
+import com.Districto_Tech.distribuidora.common.config.MapperConfig;
+import com.Districto_Tech.distribuidora.features.payments.dto.PaymentRequestDto;
+import com.Districto_Tech.distribuidora.features.payments.dto.PaymentResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+public class PaymentMapper implements IModelMapper<Payment, PaymentResponseDto, PaymentRequestDto> {
+
+    private final MapperConfig mapperConfig;
+
+    @Override
+    public Payment toEntity(PaymentRequestDto dto) {
+        return mapperConfig.modelMapper().map(dto, Payment.class);
+    }
+
+    @Override
+    public PaymentResponseDto toDto(Payment payment) {
+        PaymentResponseDto dto = mapperConfig.modelMapper().map(payment, PaymentResponseDto.class);
+        dto.setPaymentMethod(payment.getPaymentMethod().getName().name());
+        dto.setDiscountType(payment.getDiscountType().getName().name());
+        dto.setDiscountPercentage(payment.getDiscountType().getPercentage());
+        dto.setOrderId(payment.getOrder().getId());
+        return dto;
+    }
+}
