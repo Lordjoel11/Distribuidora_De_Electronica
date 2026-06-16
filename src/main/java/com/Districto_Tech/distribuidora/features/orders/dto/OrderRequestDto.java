19cdc9eb6e9f3d7b43a11388c251e35555c02ff6
macb9613-tech <macb9613@gmail.com>
2026-06-16 01:01:43 -0300

    arreglos de errores, quito la relacion de order con cliente. Arreglo en controller de order que los Delete tenian mal puesto ("/api")

--- src/main/java/com/Districto_Tech/distribuidora/features/orders/dto/OrderRequestDto.java
+++ src/main/java/com/Districto_Tech/distribuidora/features/orders/dto/OrderRequestDto.java
@@ -1,8 +1,11 @@
 package com.Districto_Tech.distribuidora.features.orders.dto;
 
+import com.Districto_Tech.distribuidora.features.clients.ClientEntity;
+import com.Districto_Tech.distribuidora.features.employees.EmployeeEntity;
 import com.Districto_Tech.distribuidora.features.orders_details.dto.OrderDetailsRequestDto;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import java.util.List;
 
@@ -14,11 +17,11 @@ import java.util.List;
 
 public class OrderRequestDto {
 
-//    @NotNull(message = "The order must be related to an existing client.")
-//    private ClientEntity clientEntity;
-//
-//    @NotNull(message = "The order must be related to an existing employee.")
-//    private EmployeeEntity employeeEntity;
+    @NotNull(message = "The order must be related to an existing client.")
+    private ClientEntity clientEntity;
+
+    @NotNull(message = "The order must be related to an existing employee.")
+    private EmployeeEntity employeeEntity;
 
     @Valid
     @NotEmpty(message = "The order must have at least one detail.")
